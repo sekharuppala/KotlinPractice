daaf2811deb8df31b77a3b9f48b8dd175d20974e
Sekhar <sekhargowd645@gmail.com>
2026-03-23 00:35:20 +0530

    intial commit

--- src/main/kotlin/Variables.kt
+++ src/main/kotlin/Variables.kt
@@ -99,7 +99,26 @@
 //    println(number.toInt())
 ////    println(number.toDouble())
 //}
-//
+
+
+fun main(args: Array<String>) {
+val array1 = arrayOf(1,2,3,4)
+val array2 = arrayOf<Long>(11,12,13,14)
+    array1.set(0,5)
+    array1[2] = 6
+
+    array2.set(2,10)
+    array2[3] = 8
+
+    for(element in array1){
+println(element)
+    }
+    println("")
+println()
+    for(element in array2){
+println(element)
+    }
+}
 
 
 
