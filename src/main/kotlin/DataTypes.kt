e0c25a9199cbf2b5d955690c6547f29bf5c5a98b
Sekhar <sekhargowd645@gmail.com>
2026-04-06 18:05:42 +0530

    intial commit

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,88 @@
+
+/*Byte
+Short
+Int
+Long
+Float
+Double
+String
+
+
+ */
+
+
+//    fun main() {
+
+//        val char='s'
+//        println(char)
+//
+//        val s=123
+//        var boolean="true"
+//        var boolean1="false"
+//        var bankBalance=1345678373
+//        val salary=123.23
+//        val employee="123479891"
+//        val vehicleName="Honda Shine"
+//
+//
+//        val text="Hello, World"
+//        val text1="hello \njavapoint"
+//        val text2="Hello \tJavapoint"
+//        val text3="Hello \bjava language"
+//        println(text)
+//        println(text1)
+//        println(text2)
+//        println(text3)
+
+//        val name="Sekhar"
+//        val age=24
+//        val education="Btech"
+//        println("My name is $name \n My age is $age \t My education $education completed in nellores\b")
+//
+
+        /// \n = Text display in new line \\\\
+        /// \t = Text display some length space \\
+        /// \b= Text display front of the \b removes the element
+
+
+
+
+
+//    }
+//
+//fun main(){
+//    ///////Integer Types///
+//    val a: Short=100
+//    val b: Byte = 10
+//    val c: Int = 123
+//    val d: Long = 10000L
+//    ///////Float Types///
+//
+//    val e: Float = 12.3F
+//    val f: Double = 123.0
+//
+//    ////String Type/////
+//    val g: String="Sekhar"
+//
+//    ///Array Type/////
+//    val arr = arrayOf(1, 2, 3)
+//
+//
+//    //////Special Types///
+//    val x: Any = 10
+//
+//    //Unit (like void)
+//    fun test(): Unit {
+//        println("Hello")
+//    }
+//    ////Nothing (no value, used in exceptions)//
+//    fun fail(): Nothing {
+//        throw Exception("Error")
+//    }
+
+//}
+
+
+
+
+
