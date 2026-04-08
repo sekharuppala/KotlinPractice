e542572384d9e5f3cd3f1954c9eecfab28c9acd2
Sekhar <sekhargowd645@gmail.com>
2026-04-08 09:03:06 +0530

    Type Conversion commit

---
+++ src/main/kotlin/TypeConversionexamples.kt
@@ -0,0 +1,109 @@
+import java.util.Arrays
+
+///Small To Big conversion flow/// Implicit
+///Byte → Short → Int → Long → Float → Double///
+
+//////Big To Small conversion////Explicit///
+////Double → Float → Long → Int → Short → Byte/////
+
+
+//fun main(){
+//    val value=10556
+//    val value3:Long = value
+//    println(value3)//// In KOtlin dont convert implicit smaller data type into larger data type
+
+
+
+
+//    val value=10556
+//    val value3:Long = value.toLong()
+//    println(value3)///Now kotlin use these helper function used to numeric conversion
+
+
+
+//
+//    var number=9849681038
+//    val number1: String=number.toString()
+//    val number2: Short=number.toShort()
+//    val number3: Float=number.toFloat()
+//    val number4: Double=number.toDouble()
+//    val number5: Int=number.toInt()
+//    print(number1)
+//    println(number2)
+//    println(number3)
+//    println(number4)
+//    print(number5)
+
+
+//    val a=1023
+////    val b: String=a.toString()
+//    val c: Long=a.toLong()
+//    val d: Float=a.toFloat()
+////    println(b)
+//    println(c)
+//    print(a)
+
+//}
+
+
+//fun main(){
+
+//    val number=12322332451
+//    val number1: String=number.toString()
+//    val number2: Float = number.toFloat()
+//    println(number1) ///// Convert from Int to Long///
+//    print(number2)
+
+
+//    val num=123.34
+//    val number: Long = num.toLong()
+//    val  number2: Int = num.toInt()
+//    println(num)
+//    println(number)
+//    println(number2)
+
+//}
+
+//fun main(){
+
+//    val num=123.3f
+//    val idLong=num.toLong()
+//    val numDouble=num.toDouble()
+//    println(idLong)
+//    println(numDouble)
+
+//    val a = 10
+//    val result = a.toLong().toDouble().toInt()
+//    println(result) ////one line chained Type conversion////
+
+//    val num=12345362712
+//    val result=num.toFloat().toDouble()
+//    val finalanswer=result.toInt().toLong()
+//    println(result)
+//    println(finalanswer)
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
