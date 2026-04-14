0cd6f81008a861472337ac2a7142ba31c69ec9f9
Sekhar <sekhargowd645@gmail.com>
2026-04-14 13:31:40 +0530

    Kotlin Loops

---
+++ src/main/kotlin/DoWhileERxamples.kt
@@ -0,0 +1,59 @@
+//fun main(){
+//    ///Syntax of the Do-While////
+//
+//    do{
+//
+//    }
+//        while (Condition);
+//
+//}
+
+//fun main(){
+////    var a=1
+////    do{
+////        println(a)
+////        a++
+////    }
+////        while (a<=5)//// Simple Program///
+//
+//
+//    do {
+//
+//        println("Enter age:")
+//        var age=readLine()!!.toInt()
+//        if (age>18){
+//            println("Eligible for Vote")
+//        } else if (age<18){
+//            println("Not Eligible For Vote")
+//
+//        }else{
+//            println("Enter only digits")
+//        }
+//    }
+//    while (age !=18)
+//
+//
+//}
+
+//fun main(){
+//    do {
+//        println("Enter a name")
+//        val name=readLine()
+//        println(name)
+//    }while (name=="sekhar")
+//    println("name are both matched")
+
+//    var a=2
+//    do {
+//        println(a)
+//        a++
+//    }
+//    while (a<=5)
+//
+//    var a=10
+//    do {
+//        println(a)
+//        a++
+//    }
+//        while (a<=5)
+//}
---
+++ src/main/kotlin/ForloopExamples.kt
@@ -0,0 +1,134 @@
+//fun main(){
+
+//    var i=10
+//    for(a in 1..i){
+//        println(a)
+//    }
+//
+//
+//    val marks=arrayOf(80,85,60,90,70)///Stores the same values////
+//    for(item in marks){
+//        println(item) ////Output:80,85,60,90,70////
+//    }
+//
+//
+//
+//}
+
+//fun main(){
+//    val marks=arrayOf("Sekhar","Nag","banu","syam")
+//    for (items in marks.indices){////Using Indices(Index)
+//        println("Marks[$items}: "+marks[items])
+
+//    }
+//
+//    print("for (i in 1..5) print(i) = ")
+//    for (i in 1..5) print(i)
+//    println()
+
+
+//}
+
+//////////////////For loop using downTo ///////
+
+//fun main(){
+//    for(i in 10 downTo 1){
+//        println(i)
+
+    /////////For loop using reverse(downTo)///
+//        for(i in 20 downTo 10){
+//            println(i)
+
+//            for(i in 1..21 step 2){
+//                println(i)///For loop using step//
+
+//
+//    val a=20
+////    val result=
+//        for(i in 15..a step 2){
+//            println(i)
+
+//    val a=10
+//    val b=20
+//        for(i in b downTo a step 2){
+//            println(i) ///Rverse the for loop ///
+//    val a=10
+//    val b=20
+//        for(i in a .. b step 2){
+//            println(i) ///for loop using range///
+//
+//
+
+//
+//        }
+//
+//    }
+
+//fun main(){
+//    for (i in start until end){
+//        ///Code
+//    }///// Syntax of the Until////
+
+
+//    for (i in 1 until 5){
+//        println(i)
+//    }
+//    for (i in 1 until  100 step 3){
+//        println(i) ////until using step////
+//    }
+//}
+
+//fun main(){
+//    val name="sekhar"
+//
+//    for(i in name.length-1 downTo 0){
+//        println(name[i])////Reverse  a string/////
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
