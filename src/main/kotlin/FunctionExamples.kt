362ea9fc1420c6727c8afd7922423f66ededb766
Sekhar <sekhargowd645@gmail.com>
2026-04-15 10:44:48 +0530

    Kotlin Functios

---
+++ src/main/kotlin/FunctionExamples.kt
@@ -0,0 +1,130 @@
+//fun main(){
+//////    sample()
+//////    addition(20,10)
+////
+////    mul(2)
+////
+////
+////}
+////
+////
+//////fun sample(){
+//////    println("Hello!World")
+//////
+//////}
+////
+//////
+//////fun addition(num1:Int,num2:Int){
+//////    println("$num1+$num2")
+//////
+//////}
+////
+////
+////fun mul(num1: Int){
+////    for(i in 1..10){
+////    println("$num1 x $i ="+num1*i)
+////
+////}
+////
+////}
+
+
+/////////////Tutorial remaining Concepts/////////
+
+
+//////KOtlin  Simple Function//////
+//fun main(){
+//    sample()
+//    println("Code after sum")
+//
+//
+//}
+//
+//fun sample()
+//{
+//    var num=10
+//    var num1=32
+//    println("Adding:"+ (num+num1))
+//
+//}
+
+/////////Kotlin parameterize function and return a value///
+//fun main(args:Array<String>){
+//   val result=sum(20,21)
+//        println(result)
+//}
+//
+//fun sum(number: Int, number2:Int): Int{
+//    val add=number+number2
+//    return  add
+//
+//}
+////Kotlin without using return////
+
+///////Syntax of the single line expression/////
+ /////////fun functionName(parameters): ReturnType = expression//
+
+//fun main() {
+//    val result = sum(20, 21)
+//    println(result)
+//
+//}
+//
+//fun sum(num:Int,num1:Int): Int=(num+num1)
+////// witout return type //////
+
+
+
+
+/////Syntax of the single line expression/////
+///fun functionName(parameters): ReturnType = expression//
+
+//fun main() {
+//    val result = square(20)
+//    println(result)
+//
+//}
+//
+//fun square(num:Int): Int=num*num////Square function////
+
+
+
+/////Syntax of the single line expression/////
+/////fun functionName(parameters): ReturnType = expression//
+//
+//fun main(){
+//    val result=even(2,3,4)
+//    println(result)
+//}
+//fun even(num: Int,num1:Int,num2:Int): Boolean=num%2==0
+
+//fun sum(a: Int, b: Int): Int {
+//    return a + b
+//}
+//
+//fun sum(a: Double, b: Double): Double {
+//    return a + b
+//}/////Function Overloading///////
+//
+//////////Using Unit Return Type/////
+//fun main(){
+//    Showmessage()
+//}
+//fun Showmessage(): Unit{
+//    println("Hello!World" )
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
