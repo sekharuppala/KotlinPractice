06f29ba115315d7977317ae967a8e2e8d499aa8a
Sekhar <sekhargowd645@gmail.com>
2026-04-16 14:14:17 +0530

    Kotlin HF Examples

---
+++ src/main/kotlin/HFExample.kt
@@ -0,0 +1,8 @@
+//fun main(){
+//    val sum = calculate(10, 5) { x, y -> x + y }
+//    println("Sum: $sum")
+//
+//    val multiply = calculate(10, 5) { x, y -> x * y }
+//    println("Multiply: $multiply")
+//
+//}
