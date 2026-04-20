c0c2a41e53571253209de8b5f56ffd91fa1c1b5f
Sekhar <sekhargowd645@gmail.com>
2026-04-20 22:45:27 +0530

    Kotlin Default Named Arguments  Examples

---
+++ src/main/kotlin/DefaultandNamedArgumentExamples.kt
@@ -0,0 +1,39 @@
+//fun greet(name: String = "Guest") {
+//    println("Hello $name")
+//}
+//
+//fun main(){
+//    greet("Sekhar")
+//    greet(name ="Smart")
+//}
+//
+//fun student(name: String = "Unknown", age: Int = 18) {
+//    println("Name: $name, Age: $age")
+//}
+//
+//fun main(){
+//    student(name ="sekhar",age =26)
+//    student(name ="NAG",age =26)
+//    student(age =26)
+//    student("NAG",26)
+//    student()////////Multiple Parameters////
+//
+//}
+
+
+//fun order(item: String, quantity: Int = 1, price: Double = 10.0) {
+//    println("$item x $quantity = ${quantity * price}")
+//}
+//
+//fun main(){
+//    order("pen",12,23.0)
+//    order("pen")
+//    order("pen",23)
+//    order(quantity = 12, price = 20.0, item = "dosth")
+//
+//
+//
+//
+//}///// Named Arguments/////
+
+
